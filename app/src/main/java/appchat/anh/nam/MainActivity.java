30af4a7f531a77399fd41f2b73b956fd1bea8c62
dangphuongnam <dangnam197@gmail.com>
2019-08-09 03:45:17 +0700

    Initial commit

---
+++ app/src/main/java/appchat/anh/nam/MainActivity.java
@@ -0,0 +1,21 @@
+package appchat.anh.nam;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        myRef.child("test").setValue("aaa");
+    }
+}
