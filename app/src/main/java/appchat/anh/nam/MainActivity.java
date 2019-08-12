b77cf294d403ade4dd1e85f88ec768557a68dc8a
hoanganh <kuanh2571997@gmail.com>
2019-08-12 13:10:05 +0700

    update

--- app/src/main/java/appchat/anh/nam/MainActivity.java
+++ app/src/main/java/appchat/anh/nam/MainActivity.java
@@ -2,40 +2,39 @@ package appchat.anh.nam;
 
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import appchat.anh.nam.login.LoginFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private int a;
-
-
-
     private static final String TAG = "MainActivity";
+    private FragmentTransaction fragmentTransaction;
+    //private FirebaseAuth firebaseAuth;
+    //private FirebaseUser mFireBaseUser;
+    //private DatabaseReference mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
-        myRef.child("test").setValue("aaa");
-        Log.d("ketqua", "onCreate: ");
+        //DatabaseReference myRef = database.getReference();
 
-        a=10;
-        a++;
-        Log.d(TAG, "onCreate: push 09/08");
+        //firebaseAuth = FirebaseAuth.getInstance();
+        //mData = FirebaseDatabase.getInstance().getReference();
 
-        int b=a;
-        Log.d(TAG, "onCreate: hoanganhktd");
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        LoginFragment loginFragment = new LoginFragment();
+        fragmentTransaction.replace(R.id.frame, loginFragment);
+        fragmentTransaction.commit();
 
-        int c = b;
-        Log.d(TAG, "onCreate: "+c);
-
-        Log.d(TAG, "onCreate: new");
     }
 }
